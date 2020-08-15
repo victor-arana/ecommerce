9ed53863f96c33dfc99e5e712ca50b7f8a61ad4d
victor-arana <victor_arana@hotmail.com>
2020-08-15 15:41:00 -0500

    :sparkle: solve exercise

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -10,12 +10,23 @@ export class ProductComponent implements OnInit {
 
   public product: Product;
   public onSale: boolean;
+  public quantity: number;
 
   constructor() { }
 
   ngOnInit(): void {
     this.product = new Product('Coca-cola', 10, 'assets/img/no-data.gif');
     this.onSale = true;
+    this.quantity = 0;
   }
+
+  addProduct(): void{
+    this.quantity = this.quantity + 1;
+  }
+
+  removeProduct(): void{
+    this.quantity = this.quantity - 1;
+  }
+
 
 }
