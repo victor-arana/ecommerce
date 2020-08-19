a3eb6dedc2408d76c096848267a1cc59d2a718b4
victor-arana <victor_arana@hotmail.com>
2020-08-19 13:57:33 -0500

    :sparkle: remove ngClass

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -11,7 +11,6 @@ export class ProductComponent implements OnInit {
   public product: Product;
   public onSale: boolean;
   public quantity: number;
-  public productClasses;
   public options: Array<string>;
 
   constructor() { }
@@ -19,11 +18,7 @@ export class ProductComponent implements OnInit {
   ngOnInit(): void {
     this.product = new Product('Coca-cola', 10, 'assets/img/no-data.gif');
     this.onSale = true;
-    this.quantity = 0;
-    this.productClasses = {
-      "onSale": this.onSale,
-      "notOnsale:": !this.onSale
-    }
+    this.quantity = 1;
     this.options = new Array<string>(10);
     for(var i = 1; i <= 20; i++){
       this.options[i] = i.toString();
