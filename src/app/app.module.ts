434059e7f42d53e759a825a0a74f8057a6da291b
victor-arana <victor_arana@hotmail.com>
2020-08-21 23:17:25 -0500

    :sparkles: adds a new form using two-way data binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { ProductComponent } from './product/product.component';
 import { ProductListComponent } from './product-list/product-list.component';
+import { CreateProductComponent } from './create-product/create-product.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductComponent,
-    ProductListComponent
+    ProductListComponent,
+    CreateProductComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/create-product/create-product.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { Product } from '../model/product';
+
+@Component({
+  selector: 'app-create-product',
+  templateUrl: './create-product.component.html',
+  styleUrls: ['./create-product.component.css']
+})
+export class CreateProductComponent {
+
+  public product: Product;
+  constructor() { 
+    this.product = new Product(1,"The bends", 1, "assets/img/no-data.gif", true, 1);
+  }
+
+}
